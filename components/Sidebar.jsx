66bd9cc03493759db93845a6c4b86c416d926aa4
Perrine <perrine.brianchon@gmail.com>
2022-01-17 20:16:24 +0100

    :sparkles: (Login) Creation du lien vers la page de Login

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,40 +1,30 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import List from '@mui/material/List';
-import Button from '@mui/material/Button';
-import SettingsIcon from '@mui/icons-material/Settings';
-import HomeIcon from '@mui/icons-material/Home';
-import { Icon } from '@mui/material';
-
-function renderRow(props) {
-  const { index, style } = props;
-
-  return (
-    <ListItem style={style} key={index} component="div" disablePadding>
-      <ListItemButton>
-        <ListItemText primary={`Item ${index + 1}`} />
-      </ListItemButton>
-    </ListItem>
-  );
-}
+import * as React from "react";
+import { Box, ListItem, ListItemText, Button } from "@mui/material";
+import Link from "next/link";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function Sidebar() {
   return (
     <Box
-      sx={{ width: '15.5vw', height: '100vh', position: 'fixed', top: 0, left: 0, zIndex: 2, backgroundColor: 'white', paddingLeft: '-3vw',display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between' }}
+      sx={{
+        width: "15.5vw",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 2,
+        backgroundColor: "white",
+        paddingLeft: "-3vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
     >
-      <Box
-        sx={{ paddingTop: '190px', width: '80%' }}
-      >
-        <ListItem 
-           >
-          <HomeIcon 
-           sx={{ marginRight: '20px' }}
-           color="primary"
-          />
+      <Box sx={{ paddingTop: "190px", width: "80%" }}>
+        <ListItem>
+          <HomeIcon sx={{ marginRight: "20px" }} color="primary" />
           <ListItemText primary="Item 1" />
         </ListItem>
         <ListItem>
@@ -44,13 +34,20 @@ export default function Sidebar() {
           <ListItemText primary="Item 3" />
         </ListItem>
         <ListItem>
-          <ListItemText primary="Item 4" />
+          <Link href="/login">
+            <a>
+              <ListItemText primary="Se connecter" />
+            </a>
+          </Link>
         </ListItem>
       </Box>
-      <Box 
-        sx={{ marginBottom: '100px' }}
-      >
-        <Button variant="borderBtn" size="small" color="primary" startIcon={<SettingsIcon />}>
+      <Box sx={{ marginBottom: "100px" }}>
+        <Button
+          variant="borderBtn"
+          size="small"
+          color="primary"
+          startIcon={<SettingsIcon />}
+        >
           Paramètres
         </Button>
       </Box>
---
+++ pages/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid, Box } from "@mui/material";
+
+const Login = () => {
+  return <Box>Login</Box>;
+};
+
+export default Login;
