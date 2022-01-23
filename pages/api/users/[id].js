8f4aa9ca15d3e8f72653d52c4928075404f189b6
Mathieu Neveu <mathieu@komeocreation.fr>
2022-01-23 20:51:45 +0100

    :recycle: /users/ files + POST from body request

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,7 +1,26 @@
 import clientPromise from "../../../lib/mongodb";
-import {ObjectId} from "mongodb";
+import { ObjectId } from "mongodb";
 
 export default function userId(req, res) {
+    const connect = async () => {
+        const client = await clientPromise;
+        const db = await client.db(process.env.MONGO_DB_NAME);
+        return db;
+    };
+
+    const getUser = async (id, db, res) => {
+        try {
+            const user = await db
+                .collection("users")
+                .find({ _id: ObjectId(id) })
+                .toArray();
+            return res.status(200).json({ user });
+        } catch (err) {
+            console.log("error : ")
+            console.log(err)
+            return res.status(404).json({ err });
+        }
+    };
 
     const deleteUser = async (id) => {
         try{
@@ -12,7 +31,7 @@ export default function userId(req, res) {
                     _id: ObjectId(id)
                 });
             // [server_logs] confirm 200 from db
-            console.log(`[200] DELETE user with id : ${req.query.userId}`);
+            console.log(`[200] DELETE user with id : ${id}`);
             // [server_logs] confirm if the item has been deleted ifnot show a warning
             user.result.n > 0 ? console.log(`[SUCCESS] deletion confirmed`) : console.log(`[WARNING] No item deleted. Bad userId ? (${id})`);
             return res.status(200).json({ user });
@@ -25,14 +44,21 @@ export default function userId(req, res) {
     };
 
     const getRoute = async (req, res) => {
+        const db = await connect();
+        const id = req.query.id.trim();
+
         switch (req.method) {
-            case 'DELETE':
-                return await deleteUser((req.query.userId).toString());
+            case "GET": {
+                const id = req.query.id;
+                return await getUser(id, db, res);
+            }
+            case "DELETE":
+                return await deleteUser(id.toString());
             default:
                 res.status(405).end('Method Not Allowed');
                 break;
         }
-    }
+    };
 
-    return getRoute(req, res)
+    return getRoute(req, res);
 }
