1132a8b51e36b5867c6b9734e8437d463019ac76
Mathieu Neveu <mathieu@komeocreation.fr>
2022-01-26 18:26:00 +0100

    🧐 select mui step 1

--- pages/home.js
+++ pages/home.js
@@ -1,24 +1,44 @@
 import React from "react";
 import Link from "next/link";
-import { Grid, TextField } from "@mui/material";
+import {Box, Grid, TextField} from "@mui/material";
 import Card from "../components/Card";
 import SelectVariants from "../components/SelectVariants";
 import Layout from "../components/Layout/Layout";
 import APIService from "../services/APIService";
+import MenuItem from "@mui/material/MenuItem";
 
 export default function Home({ resources, categories }) {
   return (
     <Layout title="Cube | Home">
       <Grid container flexDirection="column">
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-normal"
-          defaultValue="Normal"
-          variant="filled"
-          sx={{ margin: 7 }}
-        />
+        {/*<TextField*/}
+        {/*    // hiddenLabel*/}
+        {/*    // id="filled-hidden-label-normal"*/}
+        {/*    defaultValue="Normal"*/}
+        {/*    variant="filled"*/}
+        {/*    sx={{ margin: 7 }}*/}
+        {/*/>*/}
 
-        <SelectVariants />
+        {/*<SelectVariants />*/}
+
+        <Box
+            component="form"
+            sx={{ mt: 21 }}
+        >
+          <div>
+            <TextField
+              select
+              label="select"
+              value={categories}
+              >
+              {categories.map((category) => (
+                <MenuItem key={category._id} value={category._id}>
+                  {category.name}
+                </MenuItem>
+              ))}
+            </TextField>
+          </div>
+        </Box>
         <ul>
           {resources.map((resource) => {
             return (
