3a93038faef5b53356769d2523ab5e65baae34e7
Mathieu Neveu <mathieu@komeocreation.fr>
2022-01-28 18:30:10 +0100

    🧹

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,6 @@
 export function getMatchingCategories(resourceCategory, categories) {
   return categories.map((category) => {
     if (category._id === resourceCategory) {
-      // console.log("if test .map in .map");
-      // console.log(`category : ${category._id}`);
-      // console.log(`resourceCategory : ${resourceCategory}`);
-      // console.log(`MATCHING Category : ${category.name}`);
       return `${category.name} `;
     }
   });
