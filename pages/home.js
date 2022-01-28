8a55243da734f8114aee2a9be8b6450ab053be8e
Mathieu Neveu <mathieu@komeocreation.fr>
2022-01-28 17:51:37 +0100

    home flat resourceTypes

--- pages/home.js
+++ pages/home.js
@@ -14,7 +14,8 @@ import {
 } from "@mui/material";
 import Card from "../components/Card";
 import SelectVariants from "../components/SelectVariants";
-import DefaultSelect from "../components/DefautlSelect";
+import CategoriesSelect from "../components/Home/CategoriesSelect";
+import ResourceTypeSelect from "../components/Home/ResourceTypeSelect";
 import Layout from "../components/Layout/Layout";
 import apiService from "../services/apiService";
 import MenuItem from "@mui/material/MenuItem";
@@ -22,71 +23,13 @@ import FormControl from "@mui/material/FormControl";
 import theme from "../theme";
 import PropTypes from "prop-types";
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-function getStyles(name, personName, theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
 export default function Home({ resources, categories }) {
-  const [categoriesName, setCategory] = useState([]);
-
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setCategory(typeof value === "string" ? value.split(",") : value);
-  };
-
   return (
     <Layout title="Cube | Home">
       <Grid container flexDirection="column">
-        <div>
-          <FormControl sx={{ mt: 20, width: 300 }}>
-            <InputLabel id="demo-multiple-chip-label">Catégories</InputLabel>
-            <Select
-              labelId="demo-multiple-chip-label"
-              id="demo-multiple-chip"
-              multiple
-              value={categoriesName}
-              onChange={handleChange}
-              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-              renderValue={(selected) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {selected.map((value) => (
-                    <Chip key={value} label={value} />
-                  ))}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {categories.map((category) => (
-                <MenuItem
-                  key={category._id}
-                  value={category.name}
-                  style={getStyles(category.name, categoriesName, theme)}
-                >
-                  {category.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </div>
-        <DefaultSelect />
+        <CategoriesSelect categories={categories} />
+        <ResourceTypeSelect types={["association", "event"]} />
+        {/*<ResourceTypeSelect types={["association", "event"]} />*/}
         <ul>
           {resources.map((resource) => {
             return (
