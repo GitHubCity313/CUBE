55e1760eddc9c4b783862ff4be3fb10cb9fc547f
Mathieu Neveu <mathieu@komeocreation.fr>
2022-01-28 18:34:56 +0100

    scalable ResourceTypeSelect

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Grid, TextField } from "@mui/material";
+import {Grid, TextField} from "@mui/material";
 import Card from "../components/Card";
 import SelectVariants from "../components/SelectVariants";
 import CategoriesSelect from "../components/Home/CategoriesSelect";
@@ -8,72 +8,73 @@ import ResourceTypeSelect from "../components/Home/ResourceTypeSelect";
 import Layout from "../components/Layout/Layout";
 import APIService from "../services/APIService";
 import PropTypes from "prop-types";
+import {indexResourceTypes} from "../utils";
 
-export default function Home({ resources, categories }) {
+export default function Home({resources, categories, resourceTypes}) {
   return (
-    <Layout title="Cube | Home">
-      <Grid container flexDirection="column">
-        <CategoriesSelect categories={categories} />
-        <ResourceTypeSelect types={["association", "event"]} />
-        {/*<ResourceTypeSelect types={["association", "event"]} />*/}
-        <ul>
-          {resources.map((resource) => {
-            return (
-              <li key={resource._id}>
-                <Link href={`./resource/${resource._id}`}>
-                  <a>
-                    <Card resourceData={resource} categories={categories} />
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-normal"
-          defaultValue="Normal"
-          variant="filled"
-        />
+      <Layout title="Cube | Home">
+        <Grid container flexDirection="column">
+          <CategoriesSelect categories={categories}/>
+          <ResourceTypeSelect types={resourceTypes}/>
+          <ul>
+            {resources.map((resource) => {
+              return (
+                  <li key={resource._id}>
+                    <Link href={`./resource/${resource._id}`}>
+                      <a>
+                        <Card resourceData={resource} categories={categories}/>
+                      </a>
+                    </Link>
+                  </li>
+              );
+            })}
+          </ul>
+          <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+          />
 
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-normal"
-          defaultValue="Normal"
-          variant="filled"
-        />
+          <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+          />
 
-        <SelectVariants />
-        {/* <Card /> */}
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-normal"
-          defaultValue="Normal"
-          variant="filled"
-        />
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-normal"
-          defaultValue="Normal"
-          variant="filled"
-        />
+          <SelectVariants/>
+          {/* <Card /> */}
+          <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+          />
+          <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+          />
 
-        <SelectVariants />
-        {/* <Card /> */}
-        <TextField
-          hiddenLabel
-          id="filled-hidden-label-normal"
-          defaultValue="Normal"
-          variant="filled"
-        />
-      </Grid>
-    </Layout>
+          <SelectVariants/>
+          {/* <Card /> */}
+          <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+          />
+        </Grid>
+      </Layout>
   );
 }
 
 export async function getStaticProps() {
   let resources = [];
   let categories = [];
+  let resourceTypes = [];
 
   try {
     const fetchedResources = await APIService.getItems("resources");
@@ -81,6 +82,8 @@ export async function getStaticProps() {
 
     resources = await fetchedResources.data.resources;
     categories = await fetchedCategories.data.categories;
+    resourceTypes = indexResourceTypes(resources);
+
   } catch (err) {
     console.log(err);
   }
@@ -89,6 +92,7 @@ export async function getStaticProps() {
     props: {
       resources,
       categories,
+      resourceTypes,
     },
   };
 }
@@ -96,4 +100,5 @@ export async function getStaticProps() {
 Home.propTypes = {
   resources: PropTypes.array,
   categories: PropTypes.array,
+  resourceTypes: PropTypes.array,
 };
