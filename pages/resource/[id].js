2456740d79ccc1a3a622d904bb535aa1ff4de8ee
Mathieu Neveu <mathieu@komeocreation.fr>
2022-02-20 22:10:32 +0100

    Prettiered and contents map unique key

--- pages/resource/[id].js
+++ pages/resource/[id].js
@@ -1,39 +1,55 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
 import PropTypes from "prop-types";
-import {Breadcrumbs, Divider, Fab, Grid, Link, Paper, Stack, TextareaAutosize} from "@mui/material";
+import {
+  Breadcrumbs,
+  Divider,
+  Fab,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  TextareaAutosize,
+} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import apiService from "../../services/apiService";
 import Chip from "@mui/material/Chip";
-import {getMatchingCategories} from "../../utils";
-import Image from "next/image"
+import { getMatchingCategories } from "../../utils";
+import Image from "next/image";
 import commentIcone from "../../public/icones/commentIcone.svg";
 
-export default function Resource({ resource, categories, contents, comments, resourceAuthor }) {
-  const resourceCreationDate = new Date(resource.createdAt*1000);
-  const resourceUpdatedDate = new Date(resource.updatedAt*1000);
+export default function Resource({
+  resource,
+  categories,
+  contents,
+  comments,
+  resourceAuthor,
+}) {
+  const resourceCreationDate = new Date(resource.createdAt * 1000);
+  const resourceUpdatedDate = new Date(resource.updatedAt * 1000);
   const dateFormatOptions = {
     weekday: "long",
     day: "numeric",
     month: "long",
     year: "numeric",
     hour: "numeric",
-    minute: "numeric"
+    minute: "numeric",
   };
-  const formatedCreatedDate = resourceCreationDate.toLocaleString("fr-FR", dateFormatOptions);
-  const formatedUpdatedDate = resourceUpdatedDate.toLocaleString("fr-FR", dateFormatOptions);
+  const formatedCreatedDate = resourceCreationDate.toLocaleString(
+    "fr-FR",
+    dateFormatOptions
+  );
+  const formatedUpdatedDate = resourceUpdatedDate.toLocaleString(
+    "fr-FR",
+    dateFormatOptions
+  );
+  console.log("contents");
+  console.log(typeof contents);
+  console.log(contents);
   return (
     <Layout title={resource.name} withSidebar withFooter>
-      <Grid
-          container
-          flexDirection="column"
-          sx={{mt : 20 }}
-      >
-        <Grid
-            container
-            flexDirection="row"
-            sx={{mb : 3 }}
-        >
+      <Grid container flexDirection="column">
+        <Grid container flexDirection="row" sx={{ mb: 3 }}>
           <Breadcrumbs aria-label="breadcrumb">
             <Link underline="hover" color="inherit" href="/">
               Accueil
@@ -42,116 +58,100 @@ export default function Resource({ resource, categories, contents, comments, res
           </Breadcrumbs>
         </Grid>
         <Typography variant="h1">{resource.name}</Typography>
-        <Grid
-          container
-          flexDirection="row"
-          alignItems="center"
-        >
-          <Stack direction="row" spacing={1} sx={{ mr : 1.2 }}>
-            {
-              resource.categories.map(resourceCategory => {
-                const matchedCategory = getMatchingCategories(resourceCategory, categories);
-                if(matchedCategory) {
-                  return (
-                      <Chip key={resourceCategory._id} label={matchedCategory} color="primary"/>
-                  )
-                }
-              })
-            }
+        <Grid container flexDirection="row" alignItems="center">
+          <Stack direction="row" spacing={1} sx={{ mr: 1.2 }}>
+            {resource.categories.map((resourceCategory) => {
+              const matchedCategory = getMatchingCategories(
+                resourceCategory,
+                categories
+              );
+              if (matchedCategory) {
+                return (
+                  <Chip
+                    key={resourceCategory._id}
+                    label={matchedCategory}
+                    color="primary"
+                  />
+                );
+              }
+            })}
           </Stack>
           <Divider orientation="vertical" flexItem />
-          <Stack direction="row" spacing={1} sx={{ ml : 1.2, mr : 1.2 }}>
+          <Stack direction="row" spacing={1} sx={{ ml: 1.2, mr: 1.2 }}>
             <div>
-              {
-                isNaN(resourceUpdatedDate.getDate()) ?
-                    `Publié le
+              {isNaN(resourceUpdatedDate.getDate())
+                ? `Publié le
                     ${formatedCreatedDate}`
-                    :
-                    `Mis à jour le
-                    ${formatedUpdatedDate}`
-              }
-              {
-                ` par ${resourceAuthor.firstName} ${resourceAuthor.lastName}`
-              }
+                : `Mis à jour le
+                    ${formatedUpdatedDate}`}
+              {` par ${resourceAuthor.firstName} ${resourceAuthor.lastName}`}
             </div>
           </Stack>
           <Fab size="small" variant="extended" color="primary">
-            {/*<NavigationIcon sx={{ mr: 1 }} />*/}
-            + Ajouter aux favoris
+            {/*<NavigationIcon sx={{ mr: 1 }} />*/}+ Ajouter aux favoris
           </Fab>
         </Grid>
-        <Grid
-        container
-        sx={{mt: 2, mb: 2}}
-        >
+        <Grid container sx={{ mt: 2, mb: 2 }}>
           <Image
-              src={resource.thumbnail.url}
-              alt={resource.thumbnail.alt}
-              width={900}
-              height={300}
+            src={resource.thumbnail.url}
+            alt={resource.thumbnail.alt}
+            width={900}
+            height={300}
           />
         </Grid>
-        <Grid
-          container
-          >
-          {
-            contents.content.map((content) => {
-              return(
-                  <div key={content.value} className={content.type}>
-                    {content.value}
-                  </div>
-              )
-            })
-          }
+        <Grid container>
+          {contents.content.map((content) => {
+            return (
+              <div
+                key={contents.content.indexOf(content)}
+                className={content.type}
+              >
+                {content.value}
+              </div>
+            );
+          })}
         </Grid>
-        <Grid
-            container
-            sx={{mt: 2}}
-            flexDirection="column"
-        >
+        <Grid container sx={{ mt: 2 }} flexDirection="column">
           <Typography variant="h6">Poster un commentaire</Typography>
           <TextareaAutosize
-          aria-label="Laisser un commentaire"
-          minRows={3}
-          placeholder="Veuillez vous connecter pour laisser un commentaire"
-          style={{ width: "80%" }}
+            aria-label="Laisser un commentaire"
+            minRows={3}
+            placeholder="Veuillez vous connecter pour laisser un commentaire"
+            style={{ width: "80%" }}
           />
-          <Grid
-              sx={{mt: 1}}
-          >
+          <Grid sx={{ mt: 1 }}>
             <Fab size="small" variant="extended" color="primary">
               Envoyer
             </Fab>
           </Grid>
         </Grid>
-        <Grid
-            sx={{mt: 2, mb: 2}}
-            flexDirection="column"
-        >
+        <Grid sx={{ mt: 2, mb: 2 }} flexDirection="column">
           <Typography variant="h3">Commentaires</Typography>
           {comments.map((comment) => {
-            if (!comment.isModerated){
-              const createdAtDate = new Date(comment.createdAt*1000);
-              const updatedAtDate = new Date(comment.updatedAt*1000);
-              const formatedCreatedDate = createdAtDate.toLocaleString("fr-FR", dateFormatOptions)
-              const formatedUpdatedDate = updatedAtDate.toLocaleString("fr-FR", dateFormatOptions)
+            if (!comment.isModerated) {
+              const createdAtDate = new Date(comment.createdAt * 1000);
+              const updatedAtDate = new Date(comment.updatedAt * 1000);
+              const formatedCreatedDate = createdAtDate.toLocaleString(
+                "fr-FR",
+                dateFormatOptions
+              );
+              const formatedUpdatedDate = updatedAtDate.toLocaleString(
+                "fr-FR",
+                dateFormatOptions
+              );
               return (
-                  <Paper key={comment._id} elevation={6} sx={{p:2, mb: 2}}>
-                    <Image src={commentIcone} />
-                    <Typography variant="h4">{comment.title}</Typography>
-                    <Typography variant="subtitle1">{comment.author}</Typography>
-                    <Typography variant="body1">« {comment.value} »</Typography>
-                    <Grid
-                      container
-                      >
-                      {!(isNaN(updatedAtDate.getDate())) ?
-                          `Mis à jour le ${formatedUpdatedDate}.`
-                          :
-                          `Écrit le ${formatedCreatedDate}.`
-                      }
-                    </Grid>
-                  </Paper>
-              )
+                <Paper key={comment._id} elevation={6} sx={{ p: 2, mb: 2 }}>
+                  <Image src={commentIcone} />
+                  <Typography variant="h4">{comment.title}</Typography>
+                  <Typography variant="subtitle1">{comment.author}</Typography>
+                  <Typography variant="body1">« {comment.value} »</Typography>
+                  <Grid container>
+                    {!isNaN(updatedAtDate.getDate())
+                      ? `Mis à jour le ${formatedUpdatedDate}.`
+                      : `Écrit le ${formatedCreatedDate}.`}
+                  </Grid>
+                </Paper>
+              );
             }
           })}
         </Grid>
@@ -164,7 +164,10 @@ export async function getStaticProps({ params }) {
   let resource = [];
 
   try {
-    const apiSResourceRequest = await apiService.getItem("resources", params.id);
+    const apiSResourceRequest = await apiService.getItem(
+      "resources",
+      params.id
+    );
     //mongo sending it back in an array even if there is juste one item :|
     resource = apiSResourceRequest.data.resource[0];
   } catch (e) {
@@ -183,7 +186,10 @@ export async function getStaticProps({ params }) {
   let contents = [];
 
   try {
-    const contentsReq = await apiService.getItem("contents", resource.contentId);
+    const contentsReq = await apiService.getItem(
+      "contents",
+      resource.contentId
+    );
     contents = await contentsReq.data.content[0];
   } catch (e) {
     console.log(e);
@@ -207,7 +213,7 @@ export async function getStaticProps({ params }) {
       } catch (e) {
         console.log(e);
       }
-    })
+    });
   } catch (e) {
     console.log(e);
   }
@@ -227,7 +233,7 @@ export async function getStaticProps({ params }) {
       categories,
       contents,
       comments,
-      resourceAuthor
+      resourceAuthor,
     },
   };
 }
@@ -250,5 +256,5 @@ Resource.propTypes = {
   categories: PropTypes.array,
   contents: PropTypes.object,
   comments: PropTypes.array,
-  resourceAuthor: PropTypes.object
-}
+  resourceAuthor: PropTypes.object,
+};
