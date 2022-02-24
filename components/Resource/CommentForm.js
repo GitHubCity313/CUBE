7249e730a0806ce6876d0b8c4e1dd27487065478
Mathieu Neveu <mathieu@komeocreation.fr>
2022-02-24 15:10:16 +0100

    specific placeholder

--- components/Resource/CommentForm.js
+++ components/Resource/CommentForm.js
@@ -1,9 +1,15 @@
 import Typography from "@mui/material/Typography";
 import { Button, Grid, TextareaAutosize } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../../styles/CommentForm.module.css";
+import AuthContext from "../../context/authContext";
 
 export default function CommentForm() {
+  const { session, isAuthenticated } = useContext(AuthContext);
+  let placeHolderText = "Laissez votre commentaire..";
+  if (!isAuthenticated) {
+    placeHolderText = "Veuillez vous connecter pour laisser un commentaire";
+  }
   return (
     <Grid
       className={styles.grid}
@@ -16,7 +22,7 @@ export default function CommentForm() {
         className={styles.textArea}
         aria-label="Laisser un commentaire"
         minRows={3}
-        placeholder="Veuillez vous connecter pour laisser un commentaire"
+        placeholder={placeHolderText}
       />
       <Grid sx={{ mt: 1 }}>
         <Button variant="bleuBtn">Envoyer</Button>
